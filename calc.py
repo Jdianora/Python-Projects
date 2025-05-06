61ae71d00d844fe9c3447bd54342d2c75077c46f
Jdianora <joedianora@gmail.com>
2025-05-06 13:08:28 -0400

    calc.py
    I had created this project after I had finished the python course on Boot.Dev. Using the skills they had taught me, I used my resources to gather how I could make a beginner level calculator.

---
+++ calc.py
@@ -0,0 +1,38 @@
+def addition(x,y):
+    return x+y
+def subtraction (x,y):
+    return x-y
+def multiplication (x,y):
+    return x*y
+def division (x,y):
+    return x/y
+
+print ("Please Select Which Operation You'd Like To Use.")
+print ("1. Addition")
+print ("2. Subtraction")
+print ("3. Multiplication")
+print ("4. Division")
+
+while True:
+    user_select = input("Use values 1 - 4 To Select Your Operation:")
+    if user_select in ('1', '2', '3', '4'):
+        try:
+            val1 = float(input("Insert First Value:"))
+            val2 = float(input("Insert Second Value:"))
+        except ValueError:
+            print("Invalid Input. Please Enter Number.")
+            continue
+    if user_select == '1':
+        print(val1, "+", val2, "=", addition(val1, val2))
+    elif user_select == '2':
+        print(val1, "-", val2, "=", subtraction(val1, val2))
+    elif user_select == '3':
+        print(val1, "*", val2, "=", multiplication(val1, val2))
+    elif user_select == '4':
+        print(val1, "/", val2, "=", division(val1, val2))
+
+    continue_math = input("Did You Want To Continue? (yes/no)")
+    if continue_math == "no":
+        break
+    else:
+        print("Invalid Input")
